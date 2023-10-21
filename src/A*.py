74fcdbfc3071b0c0eddb27585357ceb386d0e60a
emanueledepaola <62263172+emanueledepaola@users.noreply.github.com>
2023-10-21 13:45:00 +0200

    Update A*.py

--- src/A*.py
+++ src/A*.py
@@ -1,23 +1,28 @@
-import numpy as np
-import snake
-import fruit
+# import numpy as np
+# import snake
+# import fruit
+import wall
+grid = [[0,0,0,0],[0,0,0,0,]]
 
-def find_path(default):
 
-    direction = list(np.array(fruit.position) - np.array(snake.position))
+
+
+# def find_path(default):
+#    direction = list(np.array(fruit.position) - np.array(snake.position))
     # direction = [1, 3]
 
-    print(fruit.position)
-    print(snake.position)
-    print(direction)
 
-    if   direction[1] > 0:
-        return 'UP'
-    elif direction[1] < 0:
-        return 'DOWN'
-    elif direction[0] > 0:
-        return 'RIGHT'
-    elif direction[0] < 0:
-        return 'LEFT'
-    else:
-        return default
+#    print(fruit.position)
+#    print(snake.position)
+#   print(direction)
+
+# if   direction[1] > 0:
+#        return 'UP'
+#    elif direction[1] < 0:
+#        return 'DOWN'
+#    elif direction[0] > 0:
+#        return 'RIGHT'
+#    elif direction[0] < 0:
+#        return 'LEFT'
+#    else:
+#        return default
